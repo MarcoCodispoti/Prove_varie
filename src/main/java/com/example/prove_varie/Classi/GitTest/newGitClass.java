dd9e1fdd94703a246e4331a2446d9fe9e18c76b5
MarcoCodispoti <marco01codispoti@gmail.com>
2024-10-15 21:09:07 +0200

    altri commenti

--- src/main/java/com/example/prove_varie/Classi/GitTest/newGitClass.java
+++ src/main/java/com/example/prove_varie/Classi/GitTest/newGitClass.java
@@ -13,5 +13,7 @@ Le classi modificate sono scritte in blu prima che faccio il commit
 Una volta fatto il commit torneranno ad essere nere
 
 Quando faccio il commit, per salvare il progetto anche nella repository su GitHub devo fare:
+la prima volta: Remote -> add -> Nome (nome del controllo remoto) , Location (url della repository su gitHub dove si vuole salvare il progetto)
+la seconda volta: Remote -> push ->
 
  */

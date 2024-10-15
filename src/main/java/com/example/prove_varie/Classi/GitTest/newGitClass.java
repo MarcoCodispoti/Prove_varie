70479551442c7684357925073ac08b9f0d90a7c1
MarcoCodispoti <marco01codispoti@gmail.com>
2024-10-15 21:25:32 +0200

    prova sincronizzazione con GitHub

--- src/main/java/com/example/prove_varie/Classi/GitTest/newGitClass.java
+++ src/main/java/com/example/prove_varie/Classi/GitTest/newGitClass.java
@@ -16,4 +16,6 @@ Quando faccio il commit, per salvare il progetto anche nella repository su GitHu
 la prima volta: Remote -> add -> Nome (nome del controllo remoto) , Location (url della repository su gitHub dove si vuole salvare il progetto)
 la seconda volta: Remote -> push -> push
 
+Posso gestirlo comodamente anche dalla sezione vicino al nome del progetto (Prove_Varie) , alla sua destra, c'è scritto master (per ora)
+con il simbolo tipo di un circuito (due line con dei pallini sull'estremità)
  */

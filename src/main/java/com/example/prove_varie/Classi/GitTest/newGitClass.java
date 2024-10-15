a771ee057a031fa6754e6bea2000ab4f041a22f0
MarcoCodispoti <marco01codispoti@gmail.com>
2024-10-15 21:00:50 +0200

    Ho rinominato la cartella Test in GitTest

--- src/main/java/com/example/prove_varie/Classi/GitTest/newGitClass.java
+++ src/main/java/com/example/prove_varie/Classi/GitTest/newGitClass.java
@@ -1,4 +1,4 @@
-package com.example.prove_varie.Classi.UnitTest;
+package com.example.prove_varie.Classi.GitTest;
 
 public class newGitClass {
     //prova Ho aggiunto questa classe e la sto aggiungedo alla repository su GitHub

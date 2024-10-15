08371f3d3c52f58be078519274d7b4775f5c4f2f
MarcoCodispoti <marco01codispoti@gmail.com>
2024-10-15 21:05:34 +0200

    Ho aggiunte dei commenti per spiegare cosa ho capito di Git

--- src/main/java/com/example/prove_varie/Classi/GitTest/newGitClass.java
+++ src/main/java/com/example/prove_varie/Classi/GitTest/newGitClass.java
@@ -3,3 +3,15 @@ package com.example.prove_varie.Classi.GitTest;
 public class newGitClass {
     //prova Ho aggiunto questa classe e la sto aggiungedo alla repository su GitHub
 }
+
+/*
+Osservazioni utili
+Le classi aggiunte hanno un colore verde
+-> Se faccio lo stage diventano blu
+
+Le classi modificate sono scritte in blu prima che faccio il commit
+Una volta fatto il commit torneranno ad essere nere
+
+Quando faccio il commit, per salvare il progetto anche nella repository su GitHub devo fare: 
+
+ */

2e87341e605566f7fbaacb04f4f2558389e2b43f
MarcoCodispoti <marco01codispoti@gmail.com>
2024-10-26 16:59:17 +0200

    Aggiunto la perte sul coverage

---
+++ src/main/java/com/example/prove_varie/Classi/Coverage/CoverageProva.java
@@ -0,0 +1,6 @@
+package com.example.prove_varie.Classi.Coverage;
+
+public class CoverageProva {
+    // Il Code Coverage indica la percentuale di codice testato
+
+}
--- src/main/java/com/example/prove_varie/Classi/Selenium/SeleniumTest.java
+++ src/main/java/com/example/prove_varie/Classi/Selenium/SeleniumTest.java
@@ -1,11 +1,16 @@
 package com.example.prove_varie.Classi.Selenium;
 
-import org.openqa.selenium
 
 // Seleniunm è un framework open source che supporta il testing GUI automatico
 // Non serve per il test, serve per interfacciarsi col browser
 // Permette di simulare l'utilizzo umano del software
 
 public class SeleniumTest {
+    // Nella cartella Java in test creare un altro package di nome Selenium
+    // In questo package crearne altri 2: Uno Ide e l'altro driver
+    // Nel package driver avrò il file 'browser_scelto'driver.exe
+    // Nell'altro avrò delle classi che richiameranno a quel driver: vedi esempio
+    // Può essere utile istallare i driver e le api anche per altri browser (Vedi progetto Leo e Ale)
+
 
 }

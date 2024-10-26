c4d33e0e61983548ac14818444f4288c7d2fe85b
MarcoCodispoti <marco01codispoti@gmail.com>
2024-10-26 18:38:51 +0200

    Aggiunto contenuto su SonarCloud

---
+++ src/main/java/com/example/prove_varie/Classi/SonarCloud/provaSonarCloud.java
@@ -0,0 +1,8 @@
+package com.example.prove_varie.Classi.SonarCloud;
+
+public class provaSonarCloud {
+    // Per usare Sonarcloud: Andare su "Sonarcloud.io" e fare il login con GitHub
+    // Per aggiungere un progetto di una repository essa dovrà essere publica
+    // Posso aggiungere solo repository pubbliche (Con il piano gratis)
+    // Per aggiungere una repository vai in alto a destra sul " + " e metti new -> project
+}
